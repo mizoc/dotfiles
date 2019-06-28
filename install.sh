c16a50c8164aa39f309b97a4fca0f99e72e09591
mizoc <yaesuft729@gmail.com>
2019-06-28 18:20:53 +0900

    added settings of gitconfig

--- install.sh
+++ install.sh
@@ -42,10 +42,6 @@ mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf && echo 'done'
 cd
 
-#haskell
-echo Installing haskell...
-curl -sSL https://get.haskellstack.org/ | sh && echo done
-
 #git tree
 git config --global core.pager "more" #エスケープコード認識
 git config --global core.quotepath "false" #日本語文字化け対策
